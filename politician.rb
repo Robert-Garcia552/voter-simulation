5df1b724c3c3bfa595ac196a82f8aabf5e03321e
Robert B Garcia <robgarcia552@gmail.com>
2018-09-15 01:19:23 -0400

    politician spacing resolution

--- politician.rb
+++ politician.rb
@@ -1,51 +1,51 @@
 class Politician
-    @@politicians = []
-    attr_accessor   :name,
-                    :party
+  @@politicians = []
+  attr_accessor   :name,
+                  :party
 
-    def initialize(name, party)
-        @name = name
-        @party = party
-        @@politicians << self
+  def initialize(name, party)
+    @name = name
+    @party = party
+    @@politicians << self
+  end
+
+  def self.all
+    puts "Here are your politicians:"
+    @@politicians.each do |politician|
+        puts "#{politician.name} is a #{politician.party}."
     end
+  end
 
-    def self.all
-        puts "Here are your politicians:"
-        @@politicians.each do |politician|
-            puts "#{politician.name} is a #{politician.party}."
+  def self.update(name)
+    @@politicians.each do |politician|
+      if politician.name == name
+        puts "New Name?"
+        politician.name = gets.chomp.downcase.capitalize
+        puts "New Party ( R )epublican or ( D )emocrat?"
+        party = gets.chomp.downcase
+          if party == "r"
+            politician.party = "Republican"
+          elsif party == "d"
+            politician.party = "Democrat"
+          else
+            politician.party = "Independent"
+          end
         end
+    puts "Successfully updated #{politician.name}!"
     end
+  end
 
-    def self.update(name)
-        @@politicians.each do |politician|
-            if politician.name == name
-                puts "New Name?"
-                politician.name = gets.chomp.downcase.capitalize
-                puts "New Party ( R )epublican or ( D )emocrat?"
-                party = gets.chomp.downcase
-                if party == "r"
-                    politician.party = "Republican"
-                elsif party == "d"
-                    politician.party = "Democrat"
-                else
-                    politician.party = "Independent"
-                end
-            end
-        puts "Successfully updated #{politician.name}!"
-        end
+  def self.destroy(name)
+    @@politicians.each do |politician|
+      if politician.name == name
+        puts "Are you sure you want to delete #{politician.name}, ( Y )es or ( N )o?"
+        choice = gets.chomp.downcase
+          if choice == "y"
+            @@politicians.delete(politician)
+            puts "Successfully deleted politician!"
+          end
+      end
     end
-
-    def self.destroy(name)
-        @@politicians.each do |politician|
-            if politician.name == name
-                puts "Are you sure you want to delete #{politician.name}, ( Y )es or ( N )o?"
-                choice = gets.chomp.downcase
-                if choice == "y"
-                    @@politicians.delete(politician)
-                    puts "Successfully deleted politician!"
-                end
-            end
-        end
-    end
+  end
 
 end
