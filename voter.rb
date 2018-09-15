44015b2bc9deeab6219e8e93eb0ed8785b852a8b
Robert B Garcia <robgarcia552@gmail.com>
2018-09-15 01:21:17 -0400

    voter class resolution

--- voter.rb
+++ voter.rb
@@ -1,56 +1,56 @@
 class Voter
-    @@voters = []
-    attr_accessor   :name,
-                    :politics
+  @@voters = []
+  attr_accessor   :name,
+                  :politics
 
-    def initialize(name, politics)
-        @name = name
-        @politics = politics
-        @@voters << self
-    end
+  def initialize(name, politics)
+    @name = name
+    @politics = politics
+    @@voters << self
+  end
 
-    def self.all
-        puts "Here are your voters:"
-        @@voters.each do |voter|
-            puts "#{voter.name} is a #{voter.politics}."
-        end
+  def self.all
+    puts "Here are your voters:"
+    @@voters.each do |voter|
+        puts "#{voter.name} is a #{voter.politics}."
     end
+  end
 
-    def self.update(name)
-        @@voters.each do |voter|
-            if voter.name == name
-                puts "New Name?"
-                voter.name = gets.chomp.downcase.capitalize
-                puts "New Politics ( L )iberal, ( C )onservative, ( T )ea Party, ( S )ocialist, or ( N )eutral?"
-                politics = gets.chomp.downcase
-                if politics == "l"
-                    voter.politics = "Liberal"
-                  elsif politics == "c"
-                    voter.politics = "Conservative"
-                  elsif politics == "t"
-                    voter.politics = "Tea Party"
-                  elsif politics == "s"
-                    voter.politics = "Socialist"
-                  elsif politics == "n"
-                    voter.politics = "Neutral"
-                  else
-                    voter.politics = "Undecided"
-                  end 
-            end
-        end
+  def self.update(name)
+    @@voters.each do |voter|
+      if voter.name == name
+        puts "New Name?"
+        voter.name = gets.chomp.downcase.capitalize
+        puts "New Politics ( L )iberal, ( C )onservative, ( T )ea Party, ( S )ocialist, or ( N )eutral?"
+        politics = gets.chomp.downcase
+          if politics == "l"
+            voter.politics = "Liberal"
+          elsif politics == "c"
+            voter.politics = "Conservative"
+          elsif politics == "t"
+            voter.politics = "Tea Party"
+          elsif politics == "s"
+            voter.politics = "Socialist"
+          elsif politics == "n"
+            voter.politics = "Neutral"
+          else
+            voter.politics = "Undecided"
+          end 
+      end
     end
+  end
 
-    def self.destroy(name)
-        @@voters.each do |voter|
-            if voter.name == name
-                puts "Are you sure you want to delete #{voter.name}, ( Y )es or ( N )o?"
-                choice = gets.chomp.downcase
-                if choice == "y"
-                    @@voters.delete(voter)
-                end
-            end
-        end
-        puts "Successfully deleted voter!"
-    end
+  def self.destroy(name)
+      @@voters.each do |voter|
+          if voter.name == name
+              puts "Are you sure you want to delete #{voter.name}, ( Y )es or ( N )o?"
+              choice = gets.chomp.downcase
+              if choice == "y"
+                  @@voters.delete(voter)
+              end
+          end
+      end
+      puts "Successfully deleted voter!"
+  end
 
 end
